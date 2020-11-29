2677349921d8d958600792c80ccc2af8ebcfe0ba
iamavinash14 <ak84067@gmail.com>
2020-11-29 20:58:57 +0530

    added cors and new port address

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 const winston=require('winston'); //DEFAULT LOGGER
 const express=require('express');
 const app=express();
+const config= require('config');
 
 require('./startup/logging')();
+require('./startup/cors')(app);
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 
 //SET PORT ENVIRONMENT
-const port=process.env.PORT||3000;
+const port=config.port;
 const server=app.listen(port,()=>winston.info(`Listening on port ${port}`));
 
 module.exports=server;
--- routes/movie.js
+++ routes/movie.js
@@ -4,14 +4,16 @@ const mongoose=require('mongoose');
 const express=require('express');
 const router=express.Router(); 
 const validate=require('../middleware/validate');
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 
 //GET METHOD
 router.get('/',async (req,res)=>{
-    const movies=await Movie.find().sort('title'); 
+    const movies=await Movie.find().select("-__v").sort('title'); 
     res.send(movies);
 });
 //POST METHOD
-router.post('/',validate(validateMovie),async (req,res)=>{  
+router.post('/',[auth,admin],validate(validateMovie),async (req,res)=>{  
     const genre=await Genre.findById(req.body.genreId);
     if(!genre)return res.status(400).send('Invalid Genre');
     const movie = new Movie({
@@ -29,7 +31,7 @@ router.post('/',validate(validateMovie),async (req,res)=>{
 );
 
 //PUT METHOD
-router.put('/:id',async(req,res) =>{
+router.put('/:id',[auth,admin],async(req,res) =>{
     const{error}=validate(req.body);//Validate the client movie
     if (error)return res.status(400).send(error.details[0].message); 
 
@@ -53,17 +55,17 @@ router.put('/:id',async(req,res) =>{
    res.send(movie);
 });
 //DELETE METHOD
-router.delete('/:id',async(req,res)=>{
+router.delete('/:id',[auth,admin],async(req,res)=>{
     //Look up the movie
     const movie= await Movie.findByIdAndRemove(req.params.id)
-    if(!movie)return res.status(404).send('Movie not found');
+    if(!movie)return res.status(404).send('Movie with the given ID was not found');
     //Return the movie
     res.send(movie);
 });
 router.get('/:id',async(req,res)=>{
     //Look up the movie
-   const movie= await Movie.findById(req.params.id);
-   if(!movie)return res.status(404).send('Movie not found');
+   const movie= await (await Movie.findById(req.params.id)).select("-__v");
+   if(!movie)return res.status(404).send('Movie with the given ID was not found');
     res.send(movie);
 });
 module.exports=router;
